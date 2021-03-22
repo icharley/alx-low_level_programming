f68521cf33dc22822024bd5572fbfdee887d26a9
icharley <eng.charlesegwuh@gmail.com>
2021-03-22 16:30:45 +0000

    programme of alphabetsa using putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char d;
+
+	for (d = 'a'; d <= 'z'; d++)
+	putchar(d);
+	putchar('\n');
+	return (0);
+}
