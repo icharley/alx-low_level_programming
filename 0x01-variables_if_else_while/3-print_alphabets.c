2f2aa1dfef9f6d029676b71d1af9c9c19cac68bd
icharley <eng.charlesegwuh@gmail.com>
2021-03-22 17:52:15 +0000

    alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,8 +10,13 @@ int main(void)
 	char ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++) /* print lowercase letters */
-		putchar(ch);
+	{
+	putchar(ch);
+	}
 	for (ch = 'A'; ch <= 'Z'; ch++) /* print uppercase letters */
+       	{
+       	putchar(ch);
+	}
 	putchar('\n');
 
 	return (0);
