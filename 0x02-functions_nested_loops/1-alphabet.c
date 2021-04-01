197c831258d50ad0d8b64ebf4f3f80598d72cdd5
icharley <eng.charlesegwuh@gmail.com>
2021-04-01 17:33:01 +0000

    prints the alphabet, in lowercase, followed by a new line.

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,19 @@
-#include "Holberton.h"
+#include "holberton.h"
 
-int main(void)
+/**
+ * main - Print the word print alphabet in lower case
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
 {
 	char alpha = 'a';
-	while(alpha <= 'z')
+
+	while (alpha <= 'z')
 	{
 		_putchar(alpha);
 		alpha++;
 	}
 	_putchar('\n');
-	return (0);
+	return;
 }
