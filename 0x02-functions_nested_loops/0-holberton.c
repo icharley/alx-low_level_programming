5c9b1aef89eb99eb9cb14d45cef7abdbb65609f0
icharley <eng.charlesegwuh@gmail.com>
2021-04-01 16:49:49 +0000

    program that prints Holberton

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,11 +1,17 @@
 #include "holberton.h"
 
-int main (void)
+/**
+ * main - Print the word "Holberton"
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
 {
 	char str[] = "Holberton\n";
 	int i = 0;
 
-	while(str[i] != '\0')
+	while (str[i] != '\0')
 	{
 		_putchar(str[i]);
 		i++;
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "Holberton.h"
+
+int main(void)
+{
+	char alpha = 'a';
+	while(alpha <= 'z')
+	{
+		_putchar(alpha);
+		alpha++;
+	}
+	_putchar('\n');
+	return (0);
+}
