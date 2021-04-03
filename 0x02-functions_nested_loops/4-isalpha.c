c1288f4623e96ac37bde8efe543e04425f3df77d
icharley <eng.charlesegwuh@gmail.com>
2021-04-03 06:51:04 +0000

    function that checks for alphabetic character.

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _isalpha - check for alphabet characters
+ * @c: characeter to check
+ * Return: 1 if lowercase, 0 if uppercase
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 97))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+	_putchar('\n');
+}
