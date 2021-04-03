47f8c7273cf4e05d219fd961b7b1ba1e867ca0d8
icharley <eng.charlesegwuh@gmail.com>
2021-04-03 07:36:55 +0000

    function that prints the sign of a number.

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * print_sign - print sign of a number
+ * @n: number to check
+ * Return: 1 if greater than 0, 0 if 0, -1 if less than 0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+
+	_putchar('\n');
+	return (0);
+}
