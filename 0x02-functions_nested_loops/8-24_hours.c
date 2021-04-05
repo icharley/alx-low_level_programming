134712c55dc751f0bbfa2b5597cebc2c657b7291
icharley <eng.charlesegwuh@gmail.com>
2021-04-05 08:55:46 +0000

    prints every minute of the day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - print every minute of the day
+ * Description: print minutes 00:00 to 23:59
+ */
+
+void jack_bauer(void)
+{
+	int hours;
+	int min;
+
+	for (hours = 0; hours < 24; hours++)
+	{
+		for (min = 0; min < 60; min++)
+		{
+			_putchar('0' + (hours / 10));
+			_putchar('0' + (hours % 10));
+			_putchar(':');
+			_putchar('0' + (min / 10));
+			_putchar('0' + (min % 10));
+			_putchar('\n');
+		}
+	}
+}
